8e95b2d7fb891e2adf80847f6c1c7d84dd26b092
Real Meowzer <aaronkkyip@gmail.com>
2026-04-08 18:14:01 -0400

    aaa

--- war_zone/js/entities.js
+++ war_zone/js/entities.js
@@ -294,7 +294,7 @@ function checkZombieCollision(pos, radius) {
     const pMaxY = pos.y + 1.8;
 
     for (const obs of obstacles) {
-        if (obs.isSlope) continue;
+        if (obs.isSlope || obs.isRimRubble) continue;
         if (obs.box) {
             const b = obs.box;
             // Quick XZ distance cull before full AABB test
